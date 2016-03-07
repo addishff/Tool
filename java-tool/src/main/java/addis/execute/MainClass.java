1944e5a45080e540451bb1cb647560c932998b13
huang feifeng <huangfeifeng@weidian.com>
2016-03-07 17:49:19 +0800

    修改论文 & mainClass

--- java-tool/src/main/java/addis/execute/MainClass.java
+++ java-tool/src/main/java/addis/execute/MainClass.java
@@ -1,14 +1,26 @@
 package addis.execute;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 /**
  * Created by huangfeifeng on 1/18/16.
  */
 public class MainClass {
+
+    public static void Ttest(ArrayList<Long> nums) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Boolean a = true;
+        Method addMethod = nums.getClass().getDeclaredMethod("add", Object.class);
+        System.out.println(addMethod);
+        addMethod.invoke(nums, a);
+        System.out.println(nums.size());
+        Object e = nums.get(0);
+        System.out.println(e.getClass());
+        System.out.println(nums.get(0).getClass());
+    }
+
     public static void main(String... args) throws NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
-        String abc = "abc";
-        Integer i = Integer.getInteger(abc);
-        System.out.println(i);
+        Ttest(new ArrayList());
     }
 }
